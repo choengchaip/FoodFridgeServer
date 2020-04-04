2e13bbad7fed235d7c0143eea63c9b533787e8e2
Choengchai Phachonyut <choengchai@gmail.com>
2020-04-05 00:50:27 +0700

    Fix bug

--- index.js
+++ index.js
@@ -13,14 +13,13 @@ admin.initializeApp({
 const firestore = admin.firestore()
 
 const calculateIngredient = async function () {
-  let differentDays = null;
+  let differentDays = null
   let toMessage = []
   await firestore.collection('Fridge').get().then((docs) => {
     docs.docs.forEach((d) => {
-      if(d.data().date != null && d.data().uid != null){
-        differentDays = Math.ceil((d.data().date.toDate().getTime() -  new Date().getTime())/ (1000 * 3600 * 24));
-        differentDays = differentDays
-        if(differentDays < 3 && differentDays > 0){
+      if (d.data().date != null && d.data().uid != null) {
+        differentDays = Math.ceil((d.data().date.toDate().getTime() - new Date().getTime()) / (1000 * 3600 * 24))
+        if (differentDays < 3 && differentDays > 0) {
           toMessage = [
             ...toMessage,
             {
@@ -34,46 +33,59 @@ const calculateIngredient = async function () {
     })
   })
 
-  await firestore.collection('User').get().then((docs)=>{
-    docs.docs.forEach((d)=>{
-      for(let i=0; i<toMessage.length; i++){
-        let message_title = `เร็วเข้า !!`
-        let message_body = `${toMessage[i].name} ในตู้เย็นของคุณ จะหมดอายุใน ${toMessage[i].expireIn} วัน !!`
+  await firestore.collection('User').get().then((docs) => {
+    docs.docs.forEach((d) => {
+      for (let i = 0; i < toMessage.length; i++) {
+        if(toMessage[i].uid === d.data().uid){
+          let message_title = `เร็วเข้า !!`
+          let message_body = `${toMessage[i].name} ในตู้เย็นของคุณ จะหมดอายุใน ${toMessage[i].expireIn} วัน !!`
 
-        toMessage[i] = {
-          ...toMessage[i],
-          message_token: d.data().message_token,
-          message_title: message_title,
-          message_body: message_body,
+          toMessage[i] = {
+            ...toMessage[i],
+            message_token: d.data().message_token,
+            message_title: message_title,
+            message_body: message_body
+          }
         }
       }
     })
   })
 
-  for(let i=0; i<toMessage.length; i++){
-    const body = {
-      "message_payload": {
-        "notification": {
-          "title": toMessage[i].message_title,
-          "body": toMessage[i].message_body
-        },
-        "data": {
-          "click_action": "FLUTTER_NOTIFICATION_CLICK",
-          "status": "done"
-        }
-      },
-      "priority": "high",
-      "target": toMessage[i].message_token
-    }
-
-    await axios.post('https://asia-east2-foodfridge-18df3.cloudfunctions.net/sendNotification', body)
+  for (let i = 0; i < toMessage.length; i++) {
+    await firestore.collection('Notification').add({
+      uid: toMessage[i].uid,
+      ingredient_name: toMessage[i].name,
+      expireIn: toMessage[i].expireIn,
+      created_at: new Date().getTime(),
+      is_read: false
+    })
   }
 
-  console.log("Done!!!")
+  for (let i = 0; i < toMessage.length; i++) {
+    let body = {
+      'message_payload': {
+        'notification': {
+          'title': toMessage[i].message_title,
+          'body': toMessage[i].message_body
+        },
+        'data': {
+          'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+          'status': 'done',
+          "id": "1",
+        }
+      },
+      'priority': 'high',
+      'target': toMessage[i].message_token
+    }
+
+    await axios.post('https://asia-east2-foodfridge-18df3.cloudfunctions.net/sendNotification', body, {headers: { 'Content-Type': 'application/json'}})
+  }
+
+  console.log('Done!!!')
 }
 
 app.listen(8082, () => {
   console.log('Server is running')
 
-  var j = schedule.scheduleJob('1 * * * * *', calculateIngredient)
+  var j = schedule.scheduleJob('10 * * * * *', calculateIngredient)
 })
